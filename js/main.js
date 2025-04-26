840a58d2d4a8ef059b21c1fa9f5c4d7fc0bd5a51
Michael Armstrong <mjarmstrong@habaneroconsulting.com>
2025-04-25 23:45:11 -0600

    remove dots

--- js/main.js
+++ js/main.js
@@ -21,6 +21,7 @@
     animateOut: "fadeOut",
     animateIn: "fadeIn",
     nav: true,
+    dots: false,
     autoplayHoverPause: true,
     items: 1,
     navText: ["<span class='ion-chevron-left'></span>", "<span class='ion-chevron-right'></span>"],
@@ -36,66 +37,6 @@
       2000: {
         items: 1,
         nav: true,
-      },
-    },
-  });
-
-  // owl carousel
-  var majorCarousel = $(".js-carousel-1");
-  majorCarousel.owlCarousel({
-    loop: true,
-    autoplay: true,
-    stagePadding: 7,
-    margin: 20,
-    animateOut: "fadeOut",
-    animateIn: "fadeIn",
-    nav: true,
-    autoplayHoverPause: true,
-    items: 3,
-    navText: ["<span class='ion-chevron-left'></span>", "<span class='ion-chevron-right'></span>"],
-    responsive: {
-      0: {
-        items: 1,
-        nav: false,
-      },
-      1000: {
-        items: 2,
-        nav: false,
-      },
-      2000: {
-        items: 3,
-        nav: true,
-        loop: false,
-      },
-    },
-  });
-
-  // owl carousel
-  var major2Carousel = $(".js-carousel-2");
-  major2Carousel.owlCarousel({
-    loop: true,
-    autoplay: true,
-    stagePadding: 7,
-    margin: 20,
-    animateOut: "fadeOut",
-    animateIn: "fadeIn",
-    nav: true,
-    autoplayHoverPause: true,
-    items: 4,
-    navText: ["<span class='ion-chevron-left'></span>", "<span class='ion-chevron-right'></span>"],
-    responsive: {
-      0: {
-        items: 1,
-        nav: false,
-      },
-      1000: {
-        items: 3,
-        nav: false,
-      },
-      2000: {
-        items: 4,
-        nav: true,
-        loop: false,
       },
     },
   });
